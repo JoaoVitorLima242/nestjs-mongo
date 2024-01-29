8a5eebdb0f5ebc293a0dda0958691d370e05d0f8
JoaoVitorLima242 <jlima0500@gmail.com>
2024-01-29 11:28:28 -0300

    feat: connecting to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
